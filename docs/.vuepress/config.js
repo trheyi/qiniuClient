a7ad92403bfbfd25453e11584d21e5a6488b8244
Max <shadow.iqka@qq.com>
2021-09-20 18:57:05 +0800

    更新 icon 等

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,26 +1,20 @@
 module.exports = {
-    title: '云存储管理客户端',
-    base: '/qiniuClient/',
-    description: 'qiniuClient docs',
-    themeConfig: {
-        logo: '/assets/img/logo.jpg',
-        lastUpdated: 'Last Updated',
-        nav: [
-            { text: 'github', link: 'https://github.com/willnewii/qiniuClient' },
-        ],
-        displayAllHeaders: true,
-        sidebarDepth: 1,
-        sidebar: [
-            {
-                title: '',   // 必要的
-                collapsable: false, // 可选的, 默认值是 true,
-                sidebarDepth: 1,    // 可选的, 默认值是 1
-                children: [
-                    '/',
-                    ['/CHANGLOG', '更新日志'],
-                    ['/ISSUE', '问题'],
-                ]
-            },
-        ]
-    }
+  title: '象传云盘',
+  base: '/qiniuClient/',
+  description: 'qiniuClient docs',
+  themeConfig: {
+    logo: '/assets/img/logo.jpg',
+    lastUpdated: 'Last Updated',
+    nav: [{ text: 'github', link: 'https://www.iqka.com' }],
+    displayAllHeaders: true,
+    sidebarDepth: 1,
+    sidebar: [
+      {
+        title: '', // 必要的
+        collapsable: false, // 可选的, 默认值是 true,
+        sidebarDepth: 1, // 可选的, 默认值是 1
+        children: ['/', ['/CHANGLOG', '更新日志'], ['/ISSUE', '问题']]
+      }
+    ]
+  }
 }
